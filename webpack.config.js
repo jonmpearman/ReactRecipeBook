a6ce3075e141de6701aff16091391c3549d10fa0
Jon Pearman <jpearman@sbgtv.com>
2018-11-06 13:35:24 -0800

    working on node server

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,30 @@
 const dev = process.env.NODE_ENV !== "production";
-const path = require( "path" );
-const { BundleAnalyzerPlugin } = require( "webpack-bundle-analyzer" );
-const FriendlyErrorsWebpackPlugin = require( "friendly-errors-webpack-plugin" );
+const path = require("path");
+const {BundleAnalyzerPlugin} = require("webpack-bundle-analyzer");
+const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
 
 const plugins = [
     new FriendlyErrorsWebpackPlugin(),
 ];
 
-if ( !dev ) {
-    plugins.push( new BundleAnalyzerPlugin( {
+if(!dev) {
+    plugins.push(new BundleAnalyzerPlugin({
         analyzerMode: "static",
         reportFilename: "webpack-report.html",
         openAnalyzer: false,
-    } ) );
+    }));
 }
 
 module.exports = {
     mode: dev ? "development" : "production",
-    context: path.join( __dirname, "src" ),
+    context: path.join(__dirname, "src"),
     devtool: dev ? "none" : "source-map",
     entry: {
         app: "./index.js",
     },
     resolve: {
         modules: [
-            path.resolve( "./src" ),
+            path.resolve("./src"),
             "node_modules",
         ],
     },
@@ -34,11 +34,14 @@ module.exports = {
                 test: /\.jsx?$/,
                 exclude: /(node_modules|bower_components)/,
                 loader: "babel-loader",
+                query: {
+                    presets: ['react', 'es2015', 'stage-1']
+                }
             },
         ],
     },
     output: {
-        path: path.resolve( __dirname, "dist" ),
+        path: path.resolve(__dirname, "dist"),
         filename: "[name].bundle.js",
     },
     plugins,
