8e521f72ec566c610cbe5df3e554922b505ca865
jonmpearman <celticjmp@gmail.com>
2021-07-29 22:37:22 -0700

    add theme from component library

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import Warlock from 'warlock-ui';
+import { ThemeProvider } from '@emotion/react';
+import { Button, BaseTheme, Paragraph } from 'warlock-ui';
 
 const App = () => {
-    return (<div>
-        <Warlock.Button>Recipebook</Warlock.Button>
-    </div>);
+    const [ visible, setVisible ] = useState(false);
+    
+    return (<ThemeProvider theme={BaseTheme}>
+        <Button onClick={() => setVisible(true)}>Recipebook</Button>
+        { visible ? <Paragraph>This will show once the button is clicked!</Paragraph> : null }
+    </ThemeProvider>);
 };
 
 ReactDOM.render(<App />, document.getElementById('app-root'));
